9d8ee91ac86ea9006d528cdd3410b8fefb8d777f
Axel Bagenal <axel.bagenal@gmail.com>
2022-11-22 15:49:43 +0100

    added RUD to the controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,12 +2,14 @@
 
 # :nodoc:
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show edit update delete]
+
   def index
     @tasks = Task.all
   end
 
-  def display
-    @task = Task.find(params[:id])
+  def show
+    # @task = Task.find(params[:id])
   end
 
   def new
@@ -21,7 +23,27 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+    # @task = Task.find(params[:id])
+  end
+
+  def update
+    # @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    # @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path, status: :see_other
+  end
+
   private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,22 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  # Read all
-  get 'tasks', to: 'tasks#index'
+  # # Read all
+  # get 'tasks', to: 'tasks#index'
 
-  # Create
-  get 'tasks/new', to: 'tasks#new', as: :new_task
-  post 'tasks', to: 'tasks#create'
+  # # Create
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # post 'tasks', to: 'tasks#create'
 
-  # Read one - The `display` route needs to be *after* `new` route.
-  get 'tasks/:id', to: 'tasks#display', as: :task
+  # # Read one - The `display` route needs to be *after* `new` route.
+  # get 'tasks/:id', to: 'tasks#display', as: :task
 
-  # Update
+  # # Update
   # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   # patch 'tasks/:id', to: 'tasks#update'
 
-  # Delete
-  # delete 'tasks/:id, to: 'tasks#destroy'
+  # # Delete
+  # delete 'tasks/:id', to: 'tasks#destroy'
 
-  # resources :tasks
+  resources :tasks
 end
